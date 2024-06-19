5c72263a7a072fa5c12276b827b73843b06e0792
rleitenberger <rleitenberger@directecllc.com>
2024-06-19 08:16:05 -0400

    route error?

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -6,26 +6,22 @@ import { NextRequest } from "next/server";
 import { typeDefs } from "@/graphql/typeDefs";
 import { resolvers } from "@/graphql/resolvers";
 
-let handlerPromise = (async () => {
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
     });
 
-    return startServerAndCreateNextHandler(apolloServer, {
-        context: async (req: NextRequest): Promise<{ req: NextRequest}> => {
-            /* authentication will go here */
-
-            return {
-                req,
-            }
-        },
-    });
-})();
-
-const route = async (req: NextRequest) => {
-    const handler = await handlerPromise;
-    return handler(req);
-};
 
 export { route as GET, route as POST };
+
+
+const handler = startServerAndCreateNextHandler(apolloServer, {
+    context: async (req: NextRequest) => {
+
+      return {
+        req,
+      };
+    },
+  });
+// Export the handler for GET and POST requests
+export { handler as GET, handler as POST };
